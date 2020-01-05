06962addc77bc1d8fdbb7191f51f2fb7fce2b65c
Arthur <arthurbowers@gmail.com>
2020-01-05 16:17:33 +0000

    Possible solution. W.I.P

--- two/euler2.py
+++ two/euler2.py
@@ -1,3 +1,9 @@
-def fib(x, y):
-    fibList = []
-    fibNxt = x + y
+fibList = [1, 2, 3]
+ret = 0
+for i in range(4000000):
+    for i in len()fibList:
+        possFib = i +
+        if possFib % 2 == 0:
+            fibList.append(possFib)
+ret = sum(fibList)
+print(ret)
