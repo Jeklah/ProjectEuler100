9ea5023fc7bb56b36cb85b2191f589a3cd2eadcf
Arthur Bowers <arthur.bowers@cgi.com>
2020-01-21 13:42:12 +0000

    deleting old files.

--- two/euler2.py
+++
@@ -1,9 +0,0 @@
-fibList = [1, 2, 3]
-ret = 0
-for i in range(4000000):
-    for i in len()fibList:
-        possFib = i +
-        if possFib % 2 == 0:
-            fibList.append(possFib)
-ret = sum(fibList)
-print(ret)
