eddd709b300023824febff3b31c10cb56eaee7a3
Arthur Bowers <arthur.bowers@cgi.com>
2020-01-21 13:41:21 +0000

    Renaming directories.

---
+++ 2/euler2.py
@@ -0,0 +1,9 @@
+fibList = [1, 2, 3]
+ret = 0
+for i in range(4000000):
+    for i in len()fibList:
+        possFib = i +
+        if possFib % 2 == 0:
+            fibList.append(possFib)
+ret = sum(fibList)
+print(ret)
