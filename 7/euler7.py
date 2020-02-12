a9921beff9c77263856f3b0af10650c445f417b5
Jeklah <arthurbowers@gmail.com>
2020-02-12 11:23:39 +0000

    My attempt at solving using same method but errs.

--- 7/euler7.py
+++ 7/euler7.py
@@ -1,8 +1,14 @@
 primeNr = 0
 primeCnt = 0
+prime = 0
 while primeCnt <= 10002:
     primeNr += 1
-    if (primeNr % primeNr == 0) and (primeNr % 1 == 0) and (primeNr % 2 != 0):
+    if (primeNr % primeNr == 0) \
+    and (primeNr % 1 == 0)      \
+    and (primeNr % 2 != 0)      \
+    and (primeNr % 3 != 0):
         primeCnt += 1
+        prime = primeNr
+        print(prime)
         if primeCnt == 10001:
-            print(primeNr)
+            print(prime)
