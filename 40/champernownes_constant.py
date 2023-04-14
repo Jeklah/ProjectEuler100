9a86c61916b62ab072777f0c7a6ab6642a9c448d
ArthurB <arthur.bowers@phabrix.com>
2023-04-14 09:40:37 +0100

    Formatting.

--- 40/champernownes_constant.py
+++ 40/champernownes_constant.py
@@ -5,8 +5,14 @@
 def champernownes_constant():
     """Find the value of the following expression:
     d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000"""
-    champernownes_constant = "".join(str(i) for i in range(1, 1000000))
-    return int(champernownes_constant[0]) * int(champernownes_constant[9]) * int(champernownes_constant[99]) * int(champernownes_constant[999]) * int(champernownes_constant[9999]) * int(champernownes_constant[99999]) * int(champernownes_constant[999999])
+    champernownes_const = "".join(str(i) for i in range(1, 1000000))
+    return int(champernownes_const[0]) * \
+        int(champernownes_const[9]) * \
+        int(champernownes_const[99]) * \
+        int(champernownes_const[999]) * \
+        int(champernownes_const[9999]) * \
+        int(champernownes_const[99999]) * \
+        int(champernownes_const[999999])
 
 
 if __name__ == "__main__":
